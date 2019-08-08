b2b38ba785eba2a8d07a262f42a5420d10b253af
Malcolm Gorman <malcolm.gorman@gmail.com>
2019-08-08 18:54:53 +1000

    Update util.go

--- util.go
+++ util.go
@@ -503,14 +503,14 @@ func ProgName() string {
 /*
 	Return a string with the build date/time and (seconds-ago) of the executable and where it is installed.
 */
-func BuildDateTime() (buildTime string) {
+func BuildDateTime() (buildDateTime string) {
 	executableName := os.Args[0]
 	stat, err := os.Stat(executableName)
 	if err == nil {
 		var ago time.Duration = time.Now().Sub(stat.ModTime()).Truncate(time.Second)
 		executableName = strings.Replace(executableName, ".exe", "", 1)
 		executableName = filepath.Base(executableName)
-		buildTime = fmt.Sprintf("%s.go built %s (%v ago) installed %s\n",
+		buildDateTime = fmt.Sprintf("%s.go built %s (%v ago) installed %s\n",
 			executableName, stat.ModTime().Format(time.UnixDate), ago, os.Args[0])
 	}
 	return
